46d206dfccea5ca0ce20c97b4a898e466a7407d2
Matthew Guillen <matthewguillen777@gmail.com>
2021-04-25 10:46:14 -0400

    add api dependencies and pages

---
+++ client/utils/API.js
@@ -0,0 +1,11 @@
+import axios from "axios";
+
+// eslint-disable-next-line import/no-anonymous-default-export
+export default {
+    search: function(query) {
+        return axios.get("https://www.googleapis.com/books/v1/volumes?q=" + query);
+    },
+    saveBook :function(book) {
+        return axios.post("/" + book.id);
+    }
+}
--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require("express");
-const routes = require("./routes");
 const mongoose = require("mongoose");
+const routes = require("./routes");
 const app = express();
 const PORT = process.env.PORT || 3001;
 
@@ -19,5 +19,5 @@ mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/googlebookssear
 
 // Start the API server
 app.listen(PORT, function() {
-  console.log(`==> API Server now listening on PORT ${PORT}!`);
+  console.log(`Server now listening on PORT ${PORT}!`);
 });
